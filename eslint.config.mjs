ee6ab813041f7ebd94a93517c1d63c6337237b5a
Rahul K <rahulsachin07314@gmail.com>
2025-07-20 23:06:55 +0530

    create project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint";
+import { defineConfig } from "eslint-define-config";
 
 export default defineConfig([
   // Base JavaScript setup
