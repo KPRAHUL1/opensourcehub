7fbf906e3d3a80e906819159114eeb3430fe2f84
Rahul K <rahulsachin07314@gmail.com>
2025-07-21 10:29:32 +0530

    create folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-5xl">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="min-h-screen bg-zinc-50 text-zinc-900">
+      <header className="p-4 shadow-sm border-b bg-white sticky top-0 z-10">
+        <h1 className="text-2xl font-bold">🌐 OpenSourceHub</h1>
+      </header>
+      <main className="p-6">
+        <h2 className="text-xl font-semibold mb-4">Curated Public APIs</h2>
+      </main>
+      <footer className="text-center py-4 text-sm text-zinc-500">
+        © {new Date().getFullYear()} OpenSourceHub. Built with 
+      </footer>
+    </div>
     </>
   )
 }
